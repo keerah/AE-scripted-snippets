4fd77759e781e8500b143b925f1971091d1a31bc
Keerah <9025818+keerah@users.noreply.github.com>
2022-02-22 20:50:15 +0300

    Create SubNudgeIT.jsx

---
+++ SubNudgeIT.jsx
@@ -0,0 +1,18 @@
+// Name: SubNudgeIT
+// Author: keerah.com
+// Description: Subframe layer nudge
+//
+// Put this as a scriptlet in your KBar or similar extension
+// and another version of this script with the negative value in line 3 (var = -4;)
+// on another KBar button to Nudge backwards
+
+var ff = 4; // a fraction of a frame length to nudge by, positive value to nudge forward, negative - backward
+
+var comp = app.project.activeItem;
+var lr = comp.selectedLayers[0];
+if (comp.selectedLayers.length > 0) {
+    var frl = comp.frameDuration;
+    app.beginUndoGroup("Layer Nudge");
+    lr.startTime += frl/ff;
+    app.endUndoGroup();
+} else { alert("Select a layer") }
